5ce797668602ea8d3b545280a768775f7cc139f2
Filip Petkovski <filip.petkovsky@gmail.com>
2016-08-16 11:41:25 +0200

    Fix bug in lexicon db

--- src/db/lexicon_db.py
+++ src/db/lexicon_db.py
@@ -5,24 +5,30 @@ import re
 
 from reldi_db import DB
 
+
 def regexp(expr, item):
     reg = re.compile(expr)
     return reg.search(item) is not None
 
+
 class LexiconDB(DB):
-    _instance = None
+    _instances = {
+        'hr': None,
+        'sr': None,
+        'sl': None
+    }
 
     @staticmethod
     def getInstance(language):
         database = os.path.realpath('assets/') + '/lexdb_' + language
 
-        if LexiconDB._instance is None:
-            LexiconDB._instance = LexiconDB(DB._THE_MAGIC_WORD)
-            if LexiconDB._instance._connection is None:
+        if LexiconDB._instances[language] is None:
+            LexiconDB._instances[language] = LexiconDB(DB._THE_MAGIC_WORD)
+            if LexiconDB._instances[language]._connection is None:
                 # Initialize connection
-                LexiconDB._instance._connection = sqlite3.connect(database, isolation_level=None)
-                LexiconDB._instance._connection.text_factory = str
-                LexiconDB._instance._client = LexiconDB._instance._connection.cursor()
-                LexiconDB._instance._connection.create_function("REGEXP", 2, regexp)
+                LexiconDB._instances[language]._connection = sqlite3.connect(database, isolation_level=None)
+                LexiconDB._instances[language]._connection.text_factory = str
+                LexiconDB._instances[language]._client = LexiconDB._instances[language]._connection.cursor()
+                LexiconDB._instances[language]._connection.create_function("REGEXP", 2, regexp)
 
-        return LexiconDB._instance
+        return LexiconDB._instances[language]
