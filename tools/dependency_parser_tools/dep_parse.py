1cc90a3e670404f43ea83ae6a3a19ab37f0e06e5
Haris <zenovicharis@gmail.com>
2017-02-13 17:18:55 +0100

    Implement depparser as an http service

--- tools/dependency_parser_tools/dep_parse.py
+++ tools/dependency_parser_tools/dep_parse.py
@@ -34,13 +34,14 @@ class DependecyParseCommand(Command):
         :type i: cleo.inputs.input.Input
         :type o: cleo.outputs.output.Output
         """
-
-        segmenter = Segmenter('hr')
-        tagger = Tagger('hr', segmenter)
-        lemmatiser = Lematiser('hr', segmenter, tagger)
+        language = i.get_argument('language')
         text = i.get_argument('text')
 
-        with DependencyParser('hr', lemmatiser) as parser:
-            result = parser.parse(text)
-            print result
+        segmenter = Segmenter(language)
+        tagger = Tagger(language, segmenter)
+        lemmatiser = Lematiser(language, segmenter, tagger)
+
+        parser = DependencyParser(language, lemmatiser)
+        result = parser.parse(text)
+        print result
             # print jsonTCF('hr', text, result, lemma_idx=1, tag_idx=2, depparse_idx=3)
