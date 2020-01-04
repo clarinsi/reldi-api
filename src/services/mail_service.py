17cb9eacc47c3a26f84b31f1339d9fd880771968
Aleksandar Milinkovic <amilinko@gmail.com>
2020-01-04 16:53:22 +0100

    Formatted email messages.

--- src/services/mail_service.py
+++ src/services/mail_service.py
@@ -1,5 +1,6 @@
 import smtplib
 from smtplib import SMTPException
+from email.mime.text import MIMEText
 from ..helpers import config
 
 
@@ -17,133 +18,96 @@ class MailService(object):
         """
         Sends out an email containing a link for confirming a registered user's email address.
         """
-        sender = 'noreply@cl-services.ijs.si'
-        receivers = [email]
+        sender = 'Project ReLDI <noreply@cl-services.ijs.si>'
+        receiver = email
+        subject = "Confirm your email address"
+        text = ("Hello {0},\n\n" +
+                "please click the following link to confirm your email address {1}").format(username, confirm_email_url)
 
-        message = """From: Project ReLDI <noreply@cl-services.ijs.si>
-MIME-Version: 1.0
-Content-type: text/plain
-To: <{1}>
-Subject: Confirm your email address
-
-Hello {0},
-
-please click the following link to confirm your email address {1}""".format(username, confirm_email_url)
-
-        self.send(message, receivers, sender)
+        self.send(self.create_message(sender, receiver, subject, text))
 
     def sendAccessRequestEmail(self, username, note, login_link):
 
         """
         Sends out an email to the administrator containing a client's request for accessing the API.
         """
-        sender = 'noreply@cl-services.ijs.si'
-        receivers = ['nljubesi@gmail.com']
-
+        sender = 'Project ReLDI <noreply@cl-services.ijs.si>'
+        receiver = 'nljubesi@gmail.com'
+        subject = "New ReLDI user"
         if note != "":
             note = "The following note has been left by the user: {0}".format(note)
 
-        message = """From: Project ReLDI <noreply@cl-services.ijs.si>
-MIME-Version: 1.0
-Content-type: text/plain
-To: <{0}>
-Subject: New ReLDI user
+        text = ("A new user with the username *{0}* has requested access.\n" +
+                "Click the link to log in and review the user details: {2}\n\n" +
+                "{1}").format(username, note, login_link)
 
-A new user with the username *{0}* has requested access.
-Click the link to log in and review the user details: {2}
-
-{1}""".format(username, note, login_link)
-
-        self.send(message, receivers, sender)
+        self.send(self.create_message(sender, receiver, subject, text))
 
     def sendUserActivatedEmail(self, username, email, login_url):
 
-        sender = 'noreply@cl-services.ijs.si'
-        receivers = [email]
+        sender = 'Project ReLDI <noreply@cl-services.ijs.si>'
+        receiver = email
+        subject = "Your ReLDI account has been activated"
+        text = ("Hello {0},\n\n" +
+                "your ReLDI account has been activated.\n\n" +
+                "Click the link to go to the login page: {2}").format(username, email, login_url)
 
-        message = """From: Project ReLDI <noreply@cl-services.ijs.si>
-MIME-Version: 1.0
-Content-type: text/plain
-To: <{1}>
-Subject: Your ReLDI account has been activated
-
-Hello {0},
-
-your ReLDI account has been activated.
-
-Click the link to go to the login page: {2}""".format(username, email, login_url)
-
-        self.send(message, receivers, sender)
-
+        self.send(self.create_message(sender, receiver, subject, text))
 
     def sendUserReactivatedEmail(self, username, email, login_url):
 
         """
         Sends out an email to the user once their account has been reactivated
         """
-        sender = 'noreply@cl-services.ijs.si'
-        receivers = [email]
+        sender = 'Project ReLDI <noreply@cl-services.ijs.si>'
+        receiver = email
+        subject = "Your ReLDI account has been re-activated"
+        text = ("Hello {0},\n\n" +
+                "your ReLDI account has been re-activated.\n\n" +
+                "Click the link to go to the login page: {2}").format(username, email, login_url)
 
-        message = """From: Project ReLDI <noreply@cl-services.ijs.si>
-MIME-Version: 1.0
-Content-type: text/plain
-To: <{1}>
-Subject: Your ReLDI account has been activated
-
-Hello {0},
-
-your ReLDI account has been re-activated.
-
-Click the link to go to the login page: {2}""".format(username, email, login_url)
-
-        self.send(message, receivers, sender)
+        self.send(self.create_message(sender, receiver, subject, text))
 
     def sendUserBlockedEmail(self, username, email):
 
         """
         Sends out an email to the user once their account has been blocked.
         """
-        sender = 'noreply@cl-services.ijs.si'
-        receivers = [email]
+        sender = 'Project ReLDI <noreply@cl-services.ijs.si>'
+        receiver = email
+        subject = "Your ReLDI account has been blocked"
+        text = ("Hello {0},\n\n" +
+                "your ReLDI account has been blocked by the administrator.").format(username, email)
 
-        message = """From: Project ReLDI <noreply@cl-services.ijs.si>
-MIME-Version: 1.0
-Content-type: text/plain
-To: <{1}>
-Subject: Your ReLDI account has been blocked
-
-Hello {0},
-
-your ReLDI account has been blocked by the administrator.""".format(username, email)
-
-        self.send(message, receivers, sender)
+        self.send(self.create_message(sender, receiver, subject, text))
 
     def sendEmailForgotPasswordEmail(self, username, email, forgot_password_email_url):
 
         """
         Sends out an email containing a password reset link
         """
-        sender = 'noreply@cl-services.ijs.si'
-        receivers = [email]
+        sender = 'Project ReLDI <noreply@cl-services.ijs.si>'
+        receiver = email
+        subject = "Reset your password"
+        text = ("Hello {0},\n\n" +
+                "please click the following link to confirm your password reset {1}").format(username, forgot_password_email_url)
 
-        message = """From: Project ReLDI <noreply@cl-services.ijs.si>
-MIME-Version: 1.0
-Content-type: text/plain
-To: <{1}>
-Subject: Reset your password
+        self.send(self.create_message(sender, receiver, subject, text))
 
-Hello {0},
-
-please click the following link to confirm your password reset {1}""".format(username, forgot_password_email_url)
-
-        self.send(message, receivers, sender)
-
-    def send(self, message, receivers, sender):
+    @staticmethod
+    def send(message):
         try:
             server = smtplib.SMTP('localhost')
-            headers = ("From: %s\r\nTo: %s\r\n\r\n" % (sender, ", ".join(receivers)))
-            payload = headers + message
-            server.sendmail(sender, receivers, payload)
-            server.quit()
+            server.sendmail(message['From'], message['To'], message.as_string())
         except SMTPException, e:
             print "Error: unable to send email " + str(e)
+        finally:
+            server.quit()
+
+    @staticmethod
+    def create_message(sender, receiver, subject, text):
+        msg = MIMEText(text)
+        msg['Subject'] = subject
+        msg['From'] = sender
+        msg['To'] = receiver
+        return msg
